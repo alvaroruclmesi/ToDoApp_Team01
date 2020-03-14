54840a718d7f41fc9a07b6b7042ad3be1d5953b2
alvaroruclmesi <43779311+alvaroruclmesi@users.noreply.github.com>
2020-03-14 20:22:45 +0100

    Add columns to DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_123406) do
+ActiveRecord::Schema.define(version: 2020_03_14_191738) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_03_02_123406) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "completed_at"
+    t.date "deadline"
+    t.string "priority"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 

71e652dc120f9451249b7b3df65932bf7f418209
raulvj <33183247+raulvj@users.noreply.github.com>
2020-03-31 10:02:06 +0200

    Add extra fields to users
    Now users have first name, last name and username. Also a few design modification were made.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :todo_lists do
     resources :todo_items do
       member do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_202241) do
+ActiveRecord::Schema.define(version: 2020_03_30_162821) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 2020_03_23_202241) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
